206af278662380dc0ac5e6ab45b896bb0cda5d62
Beer4duke <daduke.github@garliclife.com>
2014-03-07 09:56:28 +0100

    example with 2 gantt charts

--- examples/simple-json-example/example.js
+++ examples/simple-json-example/example.js
@@ -13,20 +13,43 @@ function example() {
 	if (error)
 	    return console.warn(error);
 	var taskNames = [];
-console.log(json);
 	for ( var i = 0; i < json.length; i++) {
 		taskNames.push(json[i]["category"]);
 		tasks.push({
-		    "startDate" : new Date(1000*json[i]["from"]),
-		    "endDate" : new Date(1000*json[i]["to"]),
+		    "startDate" : new Date(json[i]["from"]),
+		    "endDate" : new Date(json[i]["to"]),
 		    "taskName" : json[i]["category"],
 		    "status" : json[i]["task"]
 		});
 	}
-console.log(tasks);
 	var format = "%b-%e-%y";
 	var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
-	gantt(tasks);
+	gantt(tasks, "#container1");
+});
+
+
+    var taskStatus2 = {
+        "drive 0" : "bar",
+        "drive 1" : "bar-failed",
+        "drive 2" : "bar-running"
+    };
+    var tasks2 = [];
+      d3.json("data.json", function(error, json) {
+        if (error)
+            return console.warn(error);
+        var taskNames2 = [];
+        for ( var i = 0; i < json.length; i++) {
+                taskNames2.push(json[i]["task"]);
+                tasks2.push({
+                    "startDate" : new Date(json[i]["from"]),
+                    "endDate" : new Date(json[i]["to"]),
+                    "taskName" : json[i]["task"],
+                    "status" : json[i]["category"]
+                });
+        }
+        var format = "%b-%e-%y";
+        var gantt = d3.gantt().taskTypes(taskNames2).taskStatus(taskStatus2).tickFormat(format);
+	gantt(tasks2, "#container2");
     });
 
 };
